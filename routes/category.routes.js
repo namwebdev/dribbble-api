9682aa13ade1c8a6c90797b18334daf09d6c2d2c
namwebdev <nam.job96@gmail.com>
2021-05-05 05:43:35 +0700

    - Remove all permission config

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -5,14 +5,6 @@ const {
 } = require("../controllers/category.controllers");
 const categoryRouter = express.Router();
 
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-
-const rolesHavePermission = [
-  config.user_type.ADMIN,
-  config.user_type.SUPER_ADMIN,
-];
-
 categoryRouter.get("/", getCategories);
 categoryRouter.post("/", createCategory);
 
--- routes/shot.routes.js
+++ routes/shot.routes.js
@@ -5,14 +5,6 @@ const {
 } = require("../controllers/shot.controllers");
 const shotRouter = express.Router();
 
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-
-const rolesHavePermission = [
-  config.user_type.ADMIN,
-  config.user_type.SUPER_ADMIN,
-];
-
 shotRouter.get("/", getAllShot);
 shotRouter.get("/crawl", crawlShot);
 
